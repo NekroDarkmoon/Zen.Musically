e95896f27d75f9c3c21735e4de7f6a0625225c9f
NekroDarkmoon <nekrodarkmoon@gmail.com>
2021-12-10 01:30:11 -0800

    Implemented join

--- main/commands/join.js
+++ main/commands/join.js
@@ -2,6 +2,7 @@
 //                             Imports
 // ----------------------------------------------------------------
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChannelType } from 'discord-api-types/v9';
 import {
 	CommandInteraction,
 	MessageEmbed,
@@ -13,19 +14,24 @@ import Zen from '../Zen.js';
 /**
  * @class
  */
-export default class Leave {
+export default class Join {
 	/**
 	 *
 	 * @param {Zen} bot
 	 */
 	constructor(bot) {
-		this.name = 'leave';
-		this.description = 'Leave joined voice channel.';
+		this.name = 'join';
+		this.description = 'Join a voice channel.';
 		this.global = true;
 		this.bot = bot;
 		this.data = new SlashCommandBuilder()
 			.setName(this.name)
-			.setDescription(this.description);
+			.setDescription(this.description)
+			.addChannelOption(chn => {
+				chn.setName('target').setDescription('Channel to join.');
+				chn.channelTypes = [ChannelType.GuildVoice];
+				return chn;
+			});
 	}
 
 	/**
@@ -35,28 +41,24 @@ export default class Leave {
 	execute = async interaction => {
 		// Emit Reply
 		await interaction.deferReply();
-		this.leave(interaction);
+		this.join(interaction);
 	};
 
 	/**
 	 *
 	 * @param {CommandInteraction} interaction
 	 */
-	async leave(interaction) {
+	async join(interaction) {
 		const { channel, guild, member, options } = interaction;
 		/** @type {VoiceChannel} */
-		const vChannel = member.voice.channel;
+		const vChannel = member.voice.channel || options.getChannel('target');
 		const isAdmin = member.permissions.has(Permissions.FLAGS.ADMINISTRATOR);
 
 		// Validation - Channel
 		if (!vChannel)
 			return interaction.editReply(
-				`Please join a voice channel to be able to use music commands.`
+				`Please join a voice channel to be able to use music commands or specify a channel.`
 			);
-
-		// Validation - Not In A Channel
-		if (!guild.me.voice.channel)
-			return interaction.reply(`Error: Not connected to a voice channel.`);
 		// Validation - In an other channel
 		if (vChannel?.id !== guild.me.voice.channelId && !isAdmin)
 			return interaction.editReply(
@@ -64,9 +66,8 @@ export default class Leave {
 			);
 
 		try {
-			if (this.bot.Distube.getQueue(vChannel)) this.bot.Distube.stop(vChannel);
-			else this.bot.Distube.voices.leave(guild.id);
-			return interaction.editReply(`Left Voice Channel.`);
+			this.bot.Distube.voices.join(vChannel);
+			return interaction.editReply(`Joined <#${vChannel.id}>`);
 		} catch (err) {
 			const e = new MessageEmbed()
 				.setTitle('Error')
